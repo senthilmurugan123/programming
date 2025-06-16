af2742123349bd35f6bfe76d5b9a5d2076f7e701
senthilmurugan123 <senthilmurugant9@gmail.com>
2025-06-16 20:25:09 +0530

    added string.c programing

--- string.c
+++ string.c
@@ -1,9 +1,10 @@
-#inclued<stdio.h>
+#include<stdio.h>
 void main()
 {
   // code here
- char string[10];
+  char string[10];
   scanf("%s",string);
   printf("string :%s",string);
+  printf("\nasdf");
   return 0;
 }
